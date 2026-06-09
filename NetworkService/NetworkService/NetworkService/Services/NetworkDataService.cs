8ab589ff7df832e1321cfcc78d5ed2bbe99a8f63
Predrag Radanovic <154592653+Invincible-pedkeee@users.noreply.github.com>
2026-06-09 17:43:34 +0200

    add shared network data service

---
+++ NetworkService/NetworkService/NetworkService/Services/NetworkDataService.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using NetworkService.Model;
+
+namespace NetworkService.Services
+{
+    public class NetworkDataService
+    {
+        public ObservableCollection<EntityType> EntityTypes { get; private set; }
+
+        public ObservableCollection<NetworkEntity> Entities { get; private set; }
+
+        public ObservableCollection<Measurement> Measurements { get; private set; }
+
+        public ObservableCollection<HistoryAction> HistoryActions { get; private set; }
+
+        public NetworkDataService()
+        {
+            EntityTypes = new ObservableCollection<EntityType>();
+            Entities = new ObservableCollection<NetworkEntity>();
+            Measurements = new ObservableCollection<Measurement>();
+            HistoryActions = new ObservableCollection<HistoryAction>();
+
+            LoadEntityTypes();
+            LoadInitialEntities();
+
+            AddHistory("Sistem pokrenut.");
+        }
+
+        private void LoadEntityTypes()
+        {
+            EntityTypes.Add(new EntityType("IA", "Resources/Images/road_ia.png", 15000));
+            EntityTypes.Add(new EntityType("IB", "Resources/Images/road_ib.png", 7000));
+        }
+
+        private void LoadInitialEntities()
+        {
+            Entities.Add(new NetworkEntity(1, "Autoput E75", EntityTypes[0], 0));
+            Entities.Add(new NetworkEntity(2, "Magistralni put M17", EntityTypes[1], 0));
+            Entities.Add(new NetworkEntity(3, "Put Novi Sad - Beograd", EntityTypes[0], 0));
+        }
+
+        public bool IsIdUnique(int id)
+        {
+            return !Entities.Any(entity => entity.Id == id);
+        }
+
+        public NetworkEntity GetEntityById(int id)
+        {
+            return Entities.FirstOrDefault(entity => entity.Id == id);
+        }
+
+        public NetworkEntity GetEntityBySimulatorIndex(int simulatorIndex)
+        {
+            if (simulatorIndex < 0 || simulatorIndex >= Entities.Count)
+            {
+                return null;
+            }
+
+            return Entities[simulatorIndex];
+        }
+
+        public void AddEntity(NetworkEntity entity)
+        {
+            if (entity == null)
+            {
+                return;
+            }
+
+            Entities.Add(entity);
+            AddHistory("Dodat novi entitet ID: " + entity.Id);
+        }
+
+        public void DeleteEntity(NetworkEntity entity)
+        {
+            if (entity == null)
+            {
+                return;
+            }
+
+            Entities.Remove(entity);
+            AddHistory("Obrisan entitet ID: " + entity.Id);
+        }
+
+        public Measurement AddMeasurement(NetworkEntity entity, double value)
+        {
+            if (entity == null)
+            {
+                return null;
+            }
+
+            entity.LastValue = value;
+
+            Measurement measurement = new Measurement(
+                entity.Id,
+                value,
+                DateTime.Now,
+                entity.IsValueValid);
+
+            Measurements.Add(measurement);
+
+            AddHistory("Primljeno mjerenje za ID: " + entity.Id + ", vrijednost: " + value);
+
+            return measurement;
+        }
+
+        public ObservableCollection<Measurement> GetLastMeasurementsForEntity(int entityId, int count)
+        {
+            var lastMeasurements = Measurements
+                .Where(measurement => measurement.EntityId == entityId)
+                .OrderByDescending(measurement => measurement.Timestamp)
+                .Take(count)
+                .OrderBy(measurement => measurement.Timestamp)
+                .ToList();
+
+            return new ObservableCollection<Measurement>(lastMeasurements);
+        }
+
+        public void AddHistory(string description)
+        {
+            HistoryActions.Insert(0, new HistoryAction(description));
+
+            if (HistoryActions.Count > 100)
+            {
+                HistoryActions.RemoveAt(HistoryActions.Count - 1);
+            }
+        }
+    }
+}
